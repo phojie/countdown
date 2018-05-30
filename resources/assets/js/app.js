e94059ada7bf9095371871ba9d3b1f05adcfebce
phojie <rengelphojie@yahoo.com>
2018-05-30 22:51:02 +0200

    hi

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('countdown', require('./components/countdown.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/countdown.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- <Countdown deadline="August 22, 2022"></Countdown> -->
+    <!-- or  -->
+    <Countdown end="August 26, 2018"></Countdown>
+    
+  </div>
+</template>
+
+<script>
+import Countdown from 'vuejs-countdown'
+
+export default {
+  components: { Countdown }
+}
+</script>
+
+<style>
+
+.digit{
+  color:#cddc39;
+  font-size: 50px !important;
+}
+.text{
+  font-weight: bolder
+}
+</style>
